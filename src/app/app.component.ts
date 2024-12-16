0a834f9014d5bed86a8e51e8ed27c501b7e7127d
Akanksha Chavan <akankshachavan54356@gmail.com>
2024-12-16 18:06:22 +0530

    today's update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HeroComponent } from './pages/hero/hero.component';
 import { Section1Component } from './pages/section-1/section-1.component';
 import { Section2Component } from './pages/section-2/section-2.component';
 import { Section3Component } from './pages/section-3/section-3.component';
-import { Section4Component } from './pages/section-4/section-4.component';
+// import { Section4Component } from './pages/section-4/section-4.component';
 import { Section5Component } from './pages/section-5/section-5.component';
 import { Section6Component } from './pages/section-6/section-6.component';
 import { Section7Component } from './pages/section-7/section-7.component';
@@ -23,7 +23,7 @@ import { FooterComponent } from './pages/footer/footer.component';
   imports: [RouterOutlet, CommonModule, 
     HeaderComponent, HeroComponent, 
     Section1Component, Section2Component, 
-    Section3Component, Section4Component,
+    Section3Component ,
     Section5Component, Section6Component,
     Section7Component, Section8Component,
     Section9Component, Section10Component,
