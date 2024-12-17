a7d05597a7c4a56759733de6ec410a8920bdd0cc
Akanksha Chavan <akankshachavan54356@gmail.com>
2024-12-17 10:11:21 +0530

    final commit

--- src/app/pages/section-4/section-4.component.ts
+++ src/app/pages/section-4/section-4.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-// declare var $: any;
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 
 @Component({
@@ -10,41 +10,46 @@ import { Component } from '@angular/core';
   templateUrl: './section-4.component.html',
   styleUrl: './section-4.component.scss'
 })
-export class Section4Component {
-  // ngOnInit() {
-  //   if (typeof $ !== 'undefined') {
-  //     console.log("working1",typeof $ !== 'undefined');
-  //     console.log("working");
-  //   } else {
-  //     console.error('not working 2');
-  //   }
+export class Section4Component implements OnInit {
+
+  constructor(){
+    
+  }
+
+  ngOnInit() {
+    if (typeof $ !== 'undefined') {
+      console.log("working1",typeof $ !== 'undefined');
+      console.log("working");
+    } else {
+      console.error('not working 2');
+    }
 
 
-  //   $(document).ready(function() {
-  //     $('.card-slider').slick({
-  //       slidesToShow: 3,
-  //       slidesToScroll: 1,
-  //       autoplay: true,
-  //       autoplaySpeed: 2000,
-  //       arrows: true,
-  //       responsive: [
-  //         {
-  //           breakpoint: 768,   
-  //           settings: {
-  //             slidesToShow: 1, 
-  //             slidesToScroll: 1
-  //           }
-  //         },
-  //         {
-  //           breakpoint: 1024,  
-  //           settings: {
-  //             slidesToShow: 2, 
-  //             slidesToScroll: 1
-  //           }
-  //         }
-  //       ]
-  //     });
-  //   })
-  // }
+    $(document).ready(function() {
+      $('.card-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 768,   
+            settings: {
+              slidesToShow: 1, 
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 1024,  
+            settings: {
+              slidesToShow: 2, 
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    })
+  }
 }
 
